6aba4b9735870433f8a6299371b2c3b89bdc3845
zqhai <qianhaizhang@gmail.com>
2017-01-19 15:55:53 +0800

    Enhanment:update hostip

--- r.sh
+++ r.sh
@@ -1,2 +1,2 @@
 curl -H "Content-Type: application/json" -X POST  --data '{"data":"1"}' http://10.173.32.184:8080/doc-search/data/hook
-curl -H "Content-Type: application/json" -X POST  --data '{"data":"1"}' http://10.173.32.204:8080/doc-search/data/hook
+curl -H "Content-Type: application/json" -X POST  --data '{"data":"1"}' http://10.173.32.226:8080/doc-search/data/hook
