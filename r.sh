e1e9b201f3ae6e1f0f4e52539a0bc3e2a2b4c79f
hzzhangqianhai <hzzhangqianhai>
2017-02-21 12:05:07 +0800

    Enhanment:服务迁移更换 ip

--- r.sh
+++ r.sh
@@ -1,2 +1,2 @@
-curl -H "Content-Type: application/json" -X POST  --data '{"data":"1"}' http://10.173.32.184:8080/doc-search/data/hook
-curl -H "Content-Type: application/json" -X POST  --data '{"data":"1"}' http://10.173.32.226:8080/doc-search/data/hook
+curl -H "Content-Type: application/json" -X POST  --data '{"data":"1"}' http://10.173.33.1:8080/doc-search/data/hook
+curl -H "Content-Type: application/json" -X POST  --data '{"data":"1"}' http://10.173.33.2:8080/doc-search/data/hook
